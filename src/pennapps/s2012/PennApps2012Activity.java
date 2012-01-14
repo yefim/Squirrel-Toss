4fe4e43b05372b91a0c17228acfaa02cbe91d97c
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-01-14 02:21:41 -0500

    create the HomeScreen activity for the game

--- src/pennapps/s2012/PennApps2012Activity.java
+++ src/pennapps/s2012/PennApps2012Activity.java
@@ -1,13 +1,26 @@
 package pennapps.s2012;
 
+import org.xmlpull.v1.XmlPullParser;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.Xml;
 
 public class PennApps2012Activity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        XmlPullParser parser = this.getResources().getXml(R.layout.home_screen_view);
+        AttributeSet attrs = Xml.asAttributeSet(parser);
+        
+        
+        
+        HomeScreenView hs = new HomeScreenView(this, attrs);
+        //this.setContentView(hs);
+        this.setContentView(R.layout.home_screen_view);
     }
 }
